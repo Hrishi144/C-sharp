a827956d5c992ed77e46c6cc6b2ff3a910730188
hrishicote <166132248+Hrishi144@users.noreply.github.com>
2024-11-10 12:44:42 +0530

    Add files via upload

---
+++ Frequencey.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Freaquency{
+   public class Word{
+      public static void Main(String[] args){
+        Console.WriteLine("Enter a string");
+     string word =  Console.ReadLine().ToLower();
+     Console.WriteLine("Enter the character to find the frequency");
+     char target=Char.ToLower(Console.ReadKey().KeyChar);
+     Console.WriteLine();
+     int freq=0;
+     foreach(char c in word){
+         if(c==target){
+            freq++;
+         }
+     }
+     Console.WriteLine($"The character {target} appears {freq} times");
+
+      }  
+    }
+}
